67ad6f927b513f591739b1d36d0f6bce40f36696
zxu <z18255713582@outlook.com>
2018-06-30 09:47:37 +0800

    查询省成功

--- src/utils/getResultSet.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-
-public interface getResultSet {
-    void getResultSetSuccess(ResultSet resultSet, Connection connection);
-}
